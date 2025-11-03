09de37d3e93cecfcbb7b9076011810b1ea2f64f5
YooYooY <13751840799@163.com>
2025-11-03 17:58:18 +0800

    chore: add database e-r design, update the app model default setting

--- internal/model/app.py
+++ internal/model/app.py
@@ -5,6 +5,7 @@
 @Author: 744534984cwl@gmail
 @File: app.py
 """
+
 from datetime import datetime
 from uuid import uuid4
 
@@ -16,24 +17,57 @@ from sqlalchemy import (
     Text,
     DateTime,
     Index,
+    text,
 )
 
 from internal.extension.database_extension import db
 
 
 class App(db.Model):
-    """AI应用模型"""
+    """AI application model"""
+
     __tablename__ = "app"
     __table_args__ = (
         PrimaryKeyConstraint("id", name="pk_app_id"),
         Index("idx_app_account_id", "account_id"),
     )
 
-    id = Column(UUID, default=uuid4, nullable=False)
+    id = Column(
+        UUID, default=uuid4, nullable=False, server_default=text("uuid_generate_v4()")
+    )
     account_id = Column(UUID, nullable=False)
-    name = Column(String(255), default="", nullable=False)
-    icon = Column(String(255), default="", nullable=False)
-    description = Column(Text, default="", nullable=False)
-    status = Column(String(255), default="", nullable=False)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    name = Column(
+        String(255),
+        default="",
+        nullable=False,
+        server_default=text("''::character varying"),
+    )
+    icon = Column(
+        String(255),
+        default="",
+        nullable=False,
+        server_default=text("''::character varying"),
+    )
+    description = Column(
+        Text, default="", nullable=False, server_default=text("''::text")
+    )
+    status = Column(
+        String(255),
+        default="",
+        nullable=False,
+        server_default=text("''::character varying"),
+    )
+    updated_at = Column(
+        DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP(0)"),
+        server_onupdate=text("CURRENT_TIMESTAMP(0)"),
+    )
+    created_at = Column(
+        DateTime,
+        default=datetime.now,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP(0)"),
+    )
